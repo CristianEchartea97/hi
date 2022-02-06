4dcb75faeb62c2df61593785b1751da431a99237
Frederick Alvarez <hk.fred.0@gmail.com>
2022-02-05 20:42:57 -0600

    added new forms

--- src/pages/auth/ForgotPass.vue
+++ src/pages/auth/ForgotPass.vue
@@ -1,6 +1,56 @@
 <template>
-  Forgot Password
+  <q-layout view="hHh lpR fFf">
+    <q-page-container>
+      <q-page class="myPage row items-stretch">
+        <!-- form column-->
+        <q-page-container class="cona q-pa-xl col-12 col-lg-4 row items-center">
+          <q-form @submit.prevent="submit" class="bg-white q-pa-md col-all">
+            <q-input v-model="form.email" type="email" lazy-rules label="Email"/>
+            <q-btn class="q-mt-lg" label="Recover Password" type="submit" lazy-rules color="primary"/>
+          </q-form>
+        </q-page-container>
+        <!-- video column-->
+        <q-page-container class="conb col-md-8 video shadow-10 desktop-only overflow-hidden">
+          <q-parallax :height="height">
+            <template v-slot:media>
+              <video width="1920" height="1080" poster="https://cdn.quasar.dev/img/polina.jpg" autoplay loop muted>
+                <source type="video/webm" src="https://cdn.quasar.dev/img/polina.webm">
+                <source type="video/mp4" src="https://cdn.quasar.dev/img/polina.mp4">
+              </video>
+            </template>
+          </q-parallax>
+        </q-page-container>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
+
 <script>
-export default {}
+import { ref, reactive } from 'vue'
+
+export default {
+  setup () {
+    const height = ref(window.innerHeight)
+    const submit = () => {
+      console.log('submit')
+    }
+    const form = reactive({
+      email: ''
+    })
+
+    return {
+      form,
+      submit,
+      height
+    }
+  }
+}
 </script>
+
+<style lang="sass" scoped>
+.cona
+  background: $secondary
+
+.conb
+  background: #F2C037
+</style>
