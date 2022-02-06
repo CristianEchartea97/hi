75473a4cba0e390b68f800f03929484051b1ab31
Byron Motta <michito@MacBook-Air-de-michi.local>
2022-02-06 13:07:23 -0600

    adding login validations

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -5,18 +5,36 @@
         <!-- form column-->
         <q-page-container class="cona q-pa-xl col-12 col-lg-4 row items-center">
           <q-form @submit.prevent="submit" class="bg-white q-pa-md col-all">
-            <q-input v-model="form.email" type="email" lazy-rules label="Email"/>
-            <q-input v-model="form.pass" type="password" lazy-rules label="Password"/>
-            <q-btn class="q-mt-lg" label="Login" type="submit" lazy-rules color="primary"/>
+            <q-input
+              v-model="form.email"
+              type="email"
+              lazy-rules
+              label="Email"
+              :rules="[ val => val && val.length > 0 || 'Email cannot be empty']"
+            />
+            <q-input
+              v-model="form.pass"
+              type="password"
+              lazy-rules
+              label="Password"
+              :rules="[ val => val && val.length > 0 || 'Password cannot be empty']"
+            />
+            <q-btn class="q-mt-lg" label="Login" type="submit" lazy-rules
+                   color="primary"/>
           </q-form>
         </q-page-container>
         <!-- video column-->
-        <q-page-container class="conb col-md-8 video shadow-10 desktop-only overflow-hidden">
+        <q-page-container
+          class="conb col-md-8 video shadow-10 desktop-only overflow-hidden">
           <q-parallax :height="height">
             <template v-slot:media>
-              <video width="1920" height="1080" poster="https://cdn.quasar.dev/img/polina.jpg" autoplay loop muted>
-                <source type="video/webm" src="https://cdn.quasar.dev/img/polina.webm">
-                <source type="video/mp4" src="https://cdn.quasar.dev/img/polina.mp4">
+              <video width="1920" height="1080"
+                     poster="https://cdn.quasar.dev/img/polina.jpg" autoplay
+                     loop muted>
+                <source type="video/webm"
+                        src="https://cdn.quasar.dev/img/polina.webm">
+                <source type="video/mp4"
+                        src="https://cdn.quasar.dev/img/polina.mp4">
               </video>
             </template>
           </q-parallax>
@@ -27,15 +45,19 @@
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { reactive, ref } from 'vue'
 
 export default {
-  setup (props) {
-    console.log('props: ', props)
+  setup () {
+    // console.log('props: ', props)
     const height = ref(window.innerHeight)
     const submit = () => {
-      console.log('submit')
-      this.$router.push({ name: 'home', query: { redirect: '/home' } })
+      console.log('dasdas', form.pass)
+      if (form.pass.length === 0) {
+        console.log('sip')
+        // TODO: remove
+      }
+      // this.$router.push({ name: 'home', query: { redirect: '/home' } })
     }
     const form = reactive({
       name: '',
@@ -46,7 +68,10 @@ export default {
     return {
       form,
       submit,
-      height
+      height,
+      onSubmit () {
+        alert('holis')
+      }
     }
   }
 }
