308c4a47655ccecff8ef768c8de3a74e0e5334bf
Frederick Alvarez <hk.fred.0@gmail.com>
2022-02-05 21:04:52 -0600

    added new forms

--- src/pages/files/FilesHome.vue
+++ src/pages/files/FilesHome.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="row items-center">
-    <div class="col-12">
-      <q-uploader
-        url="http://localhost:4444/upload"
-        label="Batch upload"
-        multiple
-        batch
-        style="max-width: 300px"
-      />
+  <div class="q-pa-md doc-container">
+    <div class="column items-center" style="height: 550px">
+      <div class="col">
+        <q-uploader
+          url="http://localhost:4444/upload"
+          label="Select the files to upload"
+          multiple
+          batch
+          style="max-width: 700px;min-width: 700px;"
+        />
+      </div>
     </div>
   </div>
 </template>
