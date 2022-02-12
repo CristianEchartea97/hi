9e52eeb44317c68cca423e3d68c3cb0d9830e2f1
Frederick Alvarez <hk.fred.0@gmail.com>
2022-02-12 16:18:12 -0600

    minor changes to stepper

--- src/pages/wizard/WizardHome.vue
+++ src/pages/wizard/WizardHome.vue
@@ -8,17 +8,16 @@
         :done="step > 1"
       >
         The purpose of this wizard is to help you with the upload of the information.
-        <br />
-In the next step just upload the file and we will upload it and handle the information for you.
+        <br/>
+        In the next step just upload the file and we will upload it and handle the information for you.
         <q-stepper-navigation>
-          <q-btn @click="step = 2" color="primary" label="Continue" />
+          <q-btn @click="step = 2" color="primary" label="Continue"/>
         </q-stepper-navigation>
       </q-step>
 
       <q-step
         :name="2"
         title="Add a file"
-        caption="Optional"
         icon="create_new_folder"
         :done="step > 2"
       >
@@ -27,50 +26,69 @@ In the next step just upload the file and we will upload it and handle the infor
           color="orange"
           standout
           bottom-slots
-          v-model="model"
+          v-model="file"
           label="Upload file"
+          @update:model-value="readFileProperties"
           counter
         >
           <template v-slot:prepend>
-            <q-icon name="attach_file" />
+            <q-icon name="attach_file"/>
           </template>
           <template v-slot:append>
-            <q-icon name="favorite" />
+            <q-icon name="favorite"/>
           </template>
-
-          <template v-slot:hint>Field hint</template>
         </q-file>
 
         <q-stepper-navigation>
-          <q-btn @click="step = 4" color="primary" label="Continue" />
-          <q-btn flat @click="step = 1" color="primary" label="Back" class="q-ml-sm" />
+          <q-btn @click="step = 3" color="primary" label="Continue"/>
+          <q-btn flat @click="step = 1" color="primary" label="Back" class="q-ml-sm"/>
         </q-stepper-navigation>
       </q-step>
 
       <q-step
         :name="3"
-        title="Data upload verified -disabled for now-"
+        title="Verifying file with A.I."
         icon="assignment"
-        disable
-      >This step won't show up because it is disabled.</q-step>
+      >
+        Validating the file
+        <q-stepper-navigation>
+          <q-btn @click="step = 4" color="primary" label="Continue"/>
+        </q-stepper-navigation>
+      </q-step>
 
       <q-step :name="4" title="Successful upload" icon="add_comment">
         The file was uploaded successfully.
         <q-stepper-navigation>
-          <q-btn color="primary" label="Finish" />
-          <q-btn flat @click="step = 2" color="primary" label="Back" class="q-ml-sm" />
+          <q-btn color="primary" label="Finish"/>
+          <q-btn flat @click="uploadFile" color="primary" label="Back" class="q-ml-sm"/>
         </q-stepper-navigation>
       </q-step>
     </q-stepper>
   </div>
 </template>
-
 <script>
-import { ref } from 'vue'
 export default {
-  setup () {
+  data () {
     return {
-      step: ref(1)
+      step: 1,
+      file: null,
+      alert: false
+    }
+  },
+  methods: {
+    readFileProperties () {
+      if (this.file.size < 100000) {
+        this.step = 2
+        this.$q.notify({
+          type: 'negative',
+          message: 'Your file is too small :c.'
+        })
+      }
+      console.log('File was selected ' + this.file.name)
+      console.log('File was selected ' + this.file.size)
+    },
+    uploadFile () {
+      console.log('start file upload')
     }
   }
 }
