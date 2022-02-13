975d1e31bf1aaa6b08786c829a680f58bf2a70ba
Frederick Alvarez <hk.fred.0@gmail.com>
2022-02-12 20:11:21 -0600

    added more details

--- src/store/store-app.js
+++ src/store/store-app.js
@@ -2,34 +2,24 @@ import { api } from 'boot/axios'
 
 const state = {
   token: '',
-  authenticated: false
+  authenticated: false,
+  userName: '',
+  role: ''
 }
 
 // synchronous actions
 const mutations = {
+  setRole (state, value) {
+    state.role = value
+  },
+  setUserName (state, userName) {
+    state.userName = userName
+  },
   setToken (state, token) {
     state.token = token
   },
   setAuthenticated (state, value) {
     state.authenticated = value
-  }
-}
-
-const actions = {
-  async login (context, payload) {
-    const output = await api.post('/api/login', payload)
-    const response = output.data
-    if (response.success) {
-      context.commit('setToken', response.data.token)
-      context.commit('setAuthenticated', true)
-      api.defaults.headers.common.Authorization = 'Bearer ' + response.data.token
-    }
-  },
-  async logout (context) {
-    await api.post('/api/logout')
-    context.commit('setToken', '')
-    context.commit('setAuthenticated', false)
-    api.defaults.headers.common.Authorization = ''
   }
 }
 
@@ -39,6 +29,32 @@ const getters = {
   },
   getToken: (state) => {
     return state.token
+  },
+  getUserName: (state) => {
+    return state.userName
+  },
+  getRole: (state) => {
+    return state.role
+  }
+}
+
+const actions = {
+  async login (context, payload) {
+    const output = await api.post('/api/login', payload)
+    const response = output.data
+    if (response.success) {
+      context.commit('setToken', response.data.token)
+      context.commit('setUserName', response.data.name)
+      context.commit('setRole', response.data.role)
+      context.commit('setAuthenticated', true)
+      api.defaults.headers.common.Authorization = 'Bearer ' + response.data.token
+    }
+  },
+  async logout (context) {
+    context.commit('setToken', '')
+    context.commit('setAuthenticated', false)
+    await api.post('/api/logout')
+    api.defaults.headers.common.Authorization = ''
   }
 }
 
