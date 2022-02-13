da30c654ee16946b4ffe487ace35a12798ea5451
Frederick Alvarez <hk.fred.0@gmail.com>
2022-02-12 23:59:35 -0600

    added step four

--- src/pages/wizard/StepFour.vue
+++ src/pages/wizard/StepFour.vue
@@ -21,17 +21,42 @@ export default {
     }
   },
   watch: {
-    step: function (newVal) {
+    step: async function (newVal) {
       if (newVal !== 4) {
         return
       }
       console.log('Start file upload')
-      this.fileUpload()
+      await this.fileUpload()
     }
   },
   methods: {
-    fileUpload () {
+    async fileUpload () {
       console.log('upload file ' + this.document.size)
+      const newJobOut = await this.client.post('/api/createJob')
+      const newJobData = newJobOut.data
+      if (!newJobData.success) {
+        this.$q.notify({
+          type: 'negative',
+          message: 'There was an error connecting to the server'
+        })
+        return
+      }
+      const payload = {
+        jobId: newJobData.jobId,
+        filename: this.file.name,
+        size: this.file.size
+      }
+      const newDocOut = await this.client.post('/api/registerDocument', payload)
+      const newDocData = newDocOut.data
+      if (!newDocData.success) {
+        this.$q.notify({
+          type: 'negative',
+          message: 'There was an error connecting to the server'
+        })
+      }
+      console.log(newDocData)
+      // const url = newDocData.data.url
+      // const uploadOut = await this.client.post('/api/registerDocument', payload)
     }
   }
 }
