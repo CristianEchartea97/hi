6b133f5c1ec8a6cc02e694377313cfdbd396278b
Frederick Alvarez <hk.fred.0@gmail.com>
2022-02-13 00:09:02 -0600

    added step four

--- src/pages/reports/ReportsHome.vue
+++ src/pages/reports/ReportsHome.vue
@@ -1,66 +1,84 @@
 <template>
-<q-table
-  :rows="rows"
-  :columns="columns"
-  row-key="id"
->
-  <template #top>
-    <div class="text-bold" >
-      My statements
-    </div>
-  </template>
-  <template #body-cell="props">
-    <q-td
-      :props="props"
-    >
-      <q-btn
-        flat
-        color="primary"
-        :label="props.value"
-        @click="copyToClipboard(props.value)"
-      />
-    </q-td>
-  </template>
-</q-table>
+  <q-table
+    :rows="rows"
+    :columns="columns"
+    row-key="id"
+  >
+    <template #top>
+      <div class="text-bold">
+        My statements
+      </div>
+    </template>
+    <template #body-cell="props">
+      <q-td
+        :props="props"
+      >
+        <q-btn
+          flat
+          color="primary"
+          :label="props.value"
+          @click="copyToClipboard(props.value)"
+        />
+      </q-td>
+    </template>
+  </q-table>
 </template>
 <script>
 import { copyToClipboard } from 'quasar'
-import { ref } from 'vue'
+
+const rows = [
+  {
+    id: 1,
+    name: 'Britney',
+    email: 'britney@freebritney.com',
+    details: 'button'
+  },
+  {
+    id: 2,
+    name: 'Selena Gomez',
+    email: 'selena@selenag.com',
+    details: 'button'
+  },
+  {
+    id: 3,
+    name: 'Rebeca G',
+    email: 'beckyg@rebecagar.com',
+    details: 'button'
+  }
+]
+
+const columns = [
+  {
+    label: 'Artist name',
+    field: 'name',
+    name: 'name',
+    align: 'center'
+  },
+  {
+    label: 'Email',
+    field: 'email',
+    name: 'email',
+    align: 'center'
+  },
+  {
+    label: 'Details',
+    field: 'details',
+    name: 'details',
+    align: 'right'
+  }
+]
 
 export default {
-  setup () {
-    const rows = ref([
-      {
-        id: 1,
-        name: 'Britney',
-        email: 'britney@freebritney.com',
-        details: 'button'
-      },
-      {
-        id: 2,
-        name: 'Selena Gomez',
-        email: 'selena@selenag.com',
-        details: 'button'
-      },
-      {
-        id: 3,
-        name: 'Rebeca G',
-        email: 'beckyg@rebecagar.com',
-        details: 'button'
-      }
-    ])
-
-    const columns = ref([
-      { label: 'Artist name', field: 'name', name: 'name', align: 'center' },
-      { label: 'Email', field: 'email', name: 'email', align: 'center' },
-      { label: 'Details', field: 'details', name: 'details', align: 'right' }
-    ])
-
+  data () {
     return {
       copyToClipboard,
       rows,
-      columns
+      columns,
+      client: this.api
     }
+  },
+  mounted: function () {
+    console.log('Auth ' + this.client.defaults.headers.common.Authorization)
   }
 }
 </script>
