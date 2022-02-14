01e2f00b2061ccd566ace22828ab852a53906d34
Frederick Alvarez <hk.fred.0@gmail.com>
2022-02-14 10:38:38 -0600

    better notifications

--- src/components/MessageBox.vue
+++ src/components/MessageBox.vue
@@ -5,7 +5,7 @@
       You have {{ unreadNotifications }} new messages
     </q-tooltip>
     <q-menu transition-show="jump-down">
-      <q-list bordered class="rounded-borders" style="max-width: 650px">
+      <q-list bordered class="rounded-borders" style="max-width: 650px; min-width: 650px">
         <q-item-label header>Notifications</q-item-label>
         <q-item v-show="working">
           <q-spinner-dots
@@ -13,62 +13,26 @@
             size="2em"
           />
         </q-item>
-        <q-item v-for="notification in notificationsList" clickable v-ripple :key="notification.id">
-          <q-item-section avatar>
-            <q-avatar>
-              <img alt="avatar" src="https://cdn.quasar.dev/img/avatar.png">
-            </q-avatar>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label lines="1">System Update</q-item-label>
-            <q-item-label caption lines="2">
-              <span class="text-weight-bold">{{ notification.name }}</span>
-              {{ notification.message }}
-            </q-item-label>
-          </q-item-section>
-          <q-item-section side top>
-            {{ notification.created_at }}
-          </q-item-section>
-        </q-item>
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/img/avatar2.jpg">
-            </q-avatar>
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label lines="1">Brunch this weekend?</q-item-label>
-            <q-item-label caption lines="2">
-              <span class="text-weight-bold">Janet</span>
-              -- I'll be in your neighborhood doing errands this
-              weekend. Do you want to grab brunch?
-            </q-item-label>
-          </q-item-section>
-
-          <q-item-section side top>
-            1 min ago
-          </q-item-section>
-        </q-item>
-        <q-separator inset="item"/>
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/img/avatar4.jpg">
-            </q-avatar>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label lines="1">Linear Project</q-item-label>
-            <q-item-label caption lines="2">
-              <span class="text-weight-bold">John</span>
-              -- Can we schedule a call for tomorrow?
-            </q-item-label>
-          </q-item-section>
-
-          <q-item-section side top>
-            1 min ago
-          </q-item-section>
-        </q-item>
+        <div v-for="notification in notificationsList" :key="notification.id">
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-avatar>
+                <img alt="avatar" src="https://cdn.quasar.dev/img/avatar.png">
+              </q-avatar>
+            </q-item-section>
+            <q-item-section>
+              <q-item-label lines="1">System Update</q-item-label>
+              <q-item-label caption lines="2">
+                <span class="text-weight-bold">{{ notification.name }}</span>
+                {{ notification.message }}
+              </q-item-label>
+            </q-item-section>
+            <q-item-section side top>
+              {{ notification.created_at }}
+            </q-item-section>
+          </q-item>
+          <q-separator inset="item"/>
+        </div>
       </q-list>
     </q-menu>
   </q-btn>
@@ -78,12 +42,15 @@ export default {
   data () {
     return {
       unreadNotifications: 0,
-      notificationsList: Array,
+      notificationsList: [],
       working: false
     }
   },
   methods: {
     async getNotifications () {
+      if (this.notificationsList.length > 0) {
+        return
+      }
       this.working = true
       const output = await this.api.get('/api/user/notifications/unseen')
       const response = output.data
