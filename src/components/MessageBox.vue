48247601fa6baf87979c5c6558c9fa0efcce09b6
Frederick Alvarez <hk.fred.0@gmail.com>
2022-02-14 10:26:14 -0600

    better notifications

--- src/components/MessageBox.vue
+++ src/components/MessageBox.vue
@@ -5,8 +5,31 @@
       You have {{ unreadNotifications }} new messages
     </q-tooltip>
     <q-menu transition-show="jump-down">
-      <q-list bordered class="rounded-borders" style="max-width: 350px">
-        <q-item-label header>Friends</q-item-label>
+      <q-list bordered class="rounded-borders" style="max-width: 650px">
+        <q-item-label header>Notifications</q-item-label>
+        <q-item v-show="working">
+          <q-spinner-dots
+            color="primary"
+            size="2em"
+          />
+        </q-item>
+        <q-item v-for="notification in notificationsList" clickable v-ripple :key="notification.id">
+          <q-item-section avatar>
+            <q-avatar>
+              <img alt="avatar" src="https://cdn.quasar.dev/img/avatar.png">
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label lines="1">System Update</q-item-label>
+            <q-item-label caption lines="2">
+              <span class="text-weight-bold">{{ notification.name }}</span>
+              {{ notification.message }}
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side top>
+            {{ notification.created_at }}
+          </q-item-section>
+        </q-item>
         <q-item clickable v-ripple>
           <q-item-section avatar>
             <q-avatar>
@@ -27,16 +50,13 @@
             1 min ago
           </q-item-section>
         </q-item>
-
         <q-separator inset="item"/>
-
         <q-item clickable v-ripple>
           <q-item-section avatar>
             <q-avatar>
               <img src="https://cdn.quasar.dev/img/avatar4.jpg">
             </q-avatar>
           </q-item-section>
-
           <q-item-section>
             <q-item-label lines="1">Linear Project</q-item-label>
             <q-item-label caption lines="2">
@@ -58,13 +78,16 @@ export default {
   data () {
     return {
       unreadNotifications: 0,
-      notificationsList: Array
+      notificationsList: Array,
+      working: false
     }
   },
   methods: {
     async getNotifications () {
+      this.working = true
       const output = await this.api.get('/api/user/notifications/unseen')
       const response = output.data
+      this.working = false
       this.notificationsList = response.data
     }
   },
