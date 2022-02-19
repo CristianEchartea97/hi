b2f9d86cee544945be6712bc42b6ddad111b7b04
Frederick Alvarez <hk.fred.0@gmail.com>
2022-02-19 11:07:51 -0600

    fixed

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -5,6 +5,8 @@ const api = axios.create({
   baseURL: 'http://150.136.182.124:8081/'
 })
 
+const externalApi = axios.create()
+
 // https://stackoverflow.com/questions/63100658/add-global-variable-in-vue-js-3
 export default boot(({
   app,
@@ -12,6 +14,7 @@ export default boot(({
 }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
   app.config.globalProperties.api = api
+  app.config.globalProperties.axios = externalApi
   app.config.globalProperties.author = 'donhk'
   // ^ ^ ^ this will allow you to use this.api (for Vue Options API form)
   //       so you can easily perform requests against your app's API
@@ -21,4 +24,4 @@ export default boot(({
   }
 })
 
-export { api }
+export { api, externalApi }
--- src/pages/mu/wizard/StepFour.vue
+++ src/pages/mu/wizard/StepFour.vue
@@ -8,10 +8,6 @@
     <div v-if="done">
       The file was uploaded successfully.
     </div>
-    <div v-else class="text-red">
-      <q-icon name="warning" color="warning" size="4rem"/>
-      There was a problem when trying to upload the file.
-    </div>
 
   </q-step>
 </template>
@@ -63,19 +59,28 @@ export default {
 
       const formAttr = newDocData.data.postAttr.formAttr
       const formInputs = newDocData.data.postAttr.formInputs
+      const key = newDocData.data.key
 
       const formData = new FormData()
-      formData.append('file', this.document, this.document.name)
-      formData.append('acl', formInputs.acl)
-      formData.append('key', formInputs.key)
-      formData.append('X-Amz-Credential', formInputs['X-Amz-Credential'])
-      formData.append('X-Amz-Algorithm', formInputs['X-Amz-Algorithm'])
-      formData.append('X-Amz-Date', formInputs['X-Amz-Date'])
       formData.append('Policy', formInputs.Policy)
+      formData.append('X-Amz-Algorithm', formInputs['X-Amz-Algorithm'])
+      formData.append('X-Amz-Credential', formInputs['X-Amz-Credential'])
+      formData.append('X-Amz-Date', formInputs['X-Amz-Date'])
       formData.append('X-Amz-Signature', formInputs['X-Amz-Signature'])
+      formData.append('acl', formInputs.acl)
+      formData.append('key', key)
+      formData.append('file', this.document, this.document.name)
 
       try {
-        const uploadOut = await this.api.post(formAttr.action, formData)
+        const uploadOut = await this.axios.post(formAttr.action, formData)
+        if (uploadOut.status === 204) {
+          this.done = true
+        } else {
+          this.$q.notify({
+            type: 'negative',
+            message: 'There was a problem uploading the file'
+          })
+        }
         console.log(uploadOut)
       } catch (error) {
         this.$q.notify({
