d8851e66cc32e454426df27444f26d271cbd7bee
Frederick Alvarez <hk.fred.0@gmail.com>
2022-02-19 16:40:09 -0600

    added admin route for user

--- src/pages/oa/user/UserInfo.vue
+++ src/pages/oa/user/UserInfo.vue
@@ -2,9 +2,87 @@
   <h1>user Info</h1>
 </template>
 <script>
+const columns = [
+  {
+    name: 'id',
+    required: true,
+    label: 'Job ID',
+    align: 'left',
+    field: row => row.id,
+    sortable: true
+  },
+  {
+    name: 'state',
+    label: 'State',
+    required: true,
+    align: 'center',
+    field: row => row.jobState,
+    sortable: true
+  },
+  {
+    name: 'name',
+    label: 'Doc Name',
+    required: true,
+    align: 'center',
+    field: row => row.document
+  },
+  {
+    name: 'created',
+    label: 'Created',
+    required: true,
+    align: 'center',
+    field: row => row.created_at
+  },
+  {
+    name: 'uploaded',
+    label: 'Uploaded?',
+    required: true,
+    align: 'center',
+    field: row => row.uploaded,
+    format: val => `${(val) ? 'Y' : 'N'}`
+  },
+  {
+    name: 'size',
+    required: true,
+    label: 'Size(MB)',
+    align: 'left',
+    field: row => row.size,
+    sortable: true
+  },
+  {
+    name: 'clicks',
+    required: true,
+    label: 'Clicks',
+    align: 'left',
+    field: row => row.clicks,
+    sortable: true
+  }
+]
+const rows = [
+  {
+    id: 42,
+    created_at: '2022-02-19 22:25:29',
+    jobState: 'QUEUED',
+    document: 'dummy-document.data',
+    uploaded: 1,
+    size: 52428800,
+    clicks: 0
+  },
+  {
+    id: 41,
+    created_at: '2022-02-19 19:18:30',
+    jobState: 'PREPARING',
+    document: 'dummy-document.data',
+    uploaded: 1,
+    size: 52428800,
+    clicks: 0
+  }]
 export default {
   data () {
-    return {}
+    return {
+      columns,
+      rows
+    }
   },
   beforeMount () {
     this.getUserInfo()
