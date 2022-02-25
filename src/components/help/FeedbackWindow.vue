34bbac007dfc6b327e387a3d61057b34a4677e63
Frederick Alvarez <hk.fred.0@gmail.com>
2022-02-24 20:05:48 -0600

    added buttons

---
+++ src/components/help/FeedbackWindow.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-dialog v-model="this.fixed" @hide="this.$emit('hide')">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">Feedback Window</div>
+      </q-card-section>
+
+      <q-separator/>
+
+      <q-card-section style="max-height: 50vh" class="scroll">
+        <p v-for="n in 15" :key="n">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit
+          voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem
+          aut, natus minima, porro labore.</p>
+      </q-card-section>
+
+      <q-separator/>
+
+      <q-card-actions align="right">
+        <q-btn flat label="Decline" color="primary" v-close-popup/>
+        <q-btn flat label="Accept" color="primary" v-close-popup/>
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+<script>
+export default {
+  props: {
+    openFeedback: Boolean
+  },
+  data () {
+    return {
+      fixed: false
+    }
+  },
+  methods: {},
+  watch: {
+    openFeedback: function (value) {
+      this.fixed = value
+    }
+  }
+}</script>
--- src/components/help/HelpFloatingButton.vue
+++ src/components/help/HelpFloatingButton.vue
@@ -5,17 +5,54 @@
       direction="up"
       color="primary"
     >
-      <q-fab-action @click="onClick" color="primary" icon="support_agent"/>
-      <q-fab-action @click="onClick" color="primary" icon="report_problem"/>
+      <q-fab-action @click="openSupportWindow" color="primary" icon="support_agent">
+        <q-tooltip anchor="center left"
+                   self="center right"
+                   transition-show="rotate"
+                   transition-hide="rotate"
+                   class="bg-blue"
+        >
+          Contact Support
+        </q-tooltip>
+      </q-fab-action>
+      <q-fab-action @click="openFeedbackWindow" color="primary" icon="report_problem">
+        <q-tooltip anchor="center left"
+                   self="center right"
+                   transition-show="rotate"
+                   transition-hide="rotate"
+                   class="bg-red"
+        >
+          Send Feedback
+        </q-tooltip>
+      </q-fab-action>
     </q-fab>
+    <SupportAgent :open-support="openSupport" @hide="openSupport=false"/>
+    <FeedbackWindow :open-feedback="feedbackWindow" @hide="feedbackWindow=false"/>
   </q-page-sticky>
 </template>
 <script>
+import SupportAgent from 'components/help/SupportAgent'
+import FeedbackWindow from 'components/help/FeedbackWindow'
+
 export default {
+  components: {
+    FeedbackWindow,
+    SupportAgent
+  },
+  data () {
+    return {
+      openSupport: false,
+      feedbackWindow: false
+    }
+  },
   methods: {
-    onClick () {
-      console.log('Clicked on a fab action')
-      this.fabRight = !this.fabRight
+    openSupportWindow () {
+      console.log('Support Window')
+      this.openSupport = true
+    },
+    openFeedbackWindow () {
+      console.log('Feedback Window')
+      this.feedbackWindow = true
     }
   }
 }
