107794a7934e384640bf3efc5fc98332b0573c33
Frederick Alvarez <hk.fred.0@gmail.com>
2022-03-02 22:11:24 -0600

    added animations

--- src/pages/mu/reports/ReportsHome.vue
+++ src/pages/mu/reports/ReportsHome.vue
@@ -1,21 +1,35 @@
 <template>
   <div>
-    <q-table
-      title="Reports"
-      :rows="rows"
-      :columns="columns"
-      :filter="filter"
-      :loading="loading"
-      @row-click="goToReport"
-    >
-      <template v-slot:top-right>
-        <q-input class="q-pa-md" borderless dense debounce="300" v-model="filter" placeholder="Search">
-          <template v-slot:append>
-            <q-icon name="search"/>
-          </template>
-        </q-input>
-      </template>
-    </q-table>
+    <div v-if="loading" class="row justify-center">
+      <div class="col-12 col-md-auto">
+        <q-spinner-puff size="5rem" color="cyan" />
+      </div>
+    </div>
+    <div v-if="dataAvailable">
+      <q-table
+        title="Reports"
+        :rows="rows"
+        :columns="columns"
+        :filter="filter"
+        :loading="loading"
+        @row-click="goToReport"
+      >
+        <template v-slot:top-right>
+          <q-input class="q-pa-md" borderless dense debounce="300" v-model="filter" placeholder="Search">
+            <template v-slot:append>
+              <q-icon name="search"/>
+            </template>
+          </q-input>
+        </template>
+      </q-table>
+    </div>
+    <div v-else>
+      <div class="row justify-center">
+        <div class="col-12 col-md-auto">
+          <q-img style="min-width: 600px" loading="eager" :src="emptyIcon"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -23,6 +37,8 @@
 export default {
   data () {
     return {
+      dataAvailable: false,
+      emptyIcon: null,
       loading: true,
       filter: null,
       rows: [],
@@ -56,7 +72,9 @@ export default {
       this.loading = true
       const repOut = await this.api.get('/api/mu/all/reports')
       const response = repOut.data
-      this.rows = response.data
+      this.rows = response.data.reports
+      this.emptyIcon = response.data.emptyFilesIcon
+      this.dataAvailable = this.rows.length > 0
       this.loading = false
     },
     goToReport (evt, row, index) {
