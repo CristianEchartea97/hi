282f7104e6d17cfc189d91600bc3d43011d9bcc6
Frederick Alvarez <hk.fred.0@gmail.com>
2022-03-02 18:19:46 -0600

    added minor fixes

--- src/pages/mu/bam/ReportWindow.vue
+++ src/pages/mu/bam/ReportWindow.vue
@@ -2,6 +2,7 @@
   <div v-if="!rendering">
     <bar-chart v-if="barChart.renderBar" :chartData="barChart.chartData" :options="barChart.options"/>
     <bar-chart v-if="barChart2.renderBar" :chartData="barChart2.chartData" :options="barChart2.options"/>
+    <bar-chart v-if="barChart3.renderBar" :chartData="barChart3.chartData" :options="barChart3.options"/>
   </div>
   <div v-else>
     <div class="row justify-center">
@@ -34,6 +35,11 @@ export default {
         renderBar: false,
         chartData: null,
         options: null
+      },
+      barChart3: {
+        renderBar: false,
+        chartData: null,
+        options: null
       }
     }
   },
@@ -51,9 +57,15 @@ export default {
       this.barChart.chartData = {
         datasets: [{
           data: response.data.byArtist
-        }]
+        }],
+        labels: [response.data.byArtist.filter(record => record.total > 1).map(record => record.artist)]
       }
       this.barChart.options = {
+        plugins: {
+          legend: {
+            display: false
+          }
+        },
         parsing: {
           xAxisKey: 'artist',
           yAxisKey: 'total'
@@ -63,15 +75,39 @@ export default {
       this.barChart2.chartData = {
         datasets: [{
           data: response.data.byDSP
-        }]
+        }],
+        labels: [response.data.byDSP.filter(record => record.total > 100).map(record => record.service)]
       }
       this.barChart2.options = {
+        plugins: {
+          legend: {
+            display: false
+          }
+        },
         parsing: {
           xAxisKey: 'service',
           yAxisKey: 'total'
         }
       }
       this.barChart2.renderBar = true
+      this.barChart3.chartData = {
+        datasets: [
+          {
+            label: 'population',
+            data: [30, 40, 60, 70, 5],
+            backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED']
+          }
+        ],
+        labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre']
+      }
+      this.barChart3.options = {
+        plugins: {
+          legend: {
+            display: false
+          }
+        }
+      }
+      this.barChart3.renderBar = true
       this.rendering = false
     }
   }
